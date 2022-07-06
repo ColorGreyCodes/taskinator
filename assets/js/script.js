4eff3d3461b3855f468774fc188dc1b728d97834
AyYo-Winston <105572024+AyYo-Winston@users.noreply.github.com>
2022-07-06 15:25:39 -0400

    Refractored code to organize functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,42 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     /*By adding the event argument to the createTaskHandler() function, we can use the data and functionality that object holds. We did that when we added event.preventDefault(); to the handler function's code. */
-var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";   
-    listItemE1.textContent = "This is a new task."
-    tasksToDoE1.appendChild(listItemE1);
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    console.log(taskNameInput);
+    /*If we used another set of double quotes to wrap the attribute's value, the entire string would fail because it would assume that we ended the string at "[name="; anything after would break the query selector.*/
+    var TaskTypeinput = document.querySelector("select[name = 'task-type']").value;
+    
+    //package up data as an object
+    var taskDataObject = {
+        name: taskNameInput,
+        type: TaskTypeinput
+    };
+
+    createTaskE1(taskDataObject);
+
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+var createTaskE1 = function(taskDataObj){
+    //create list item
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //Give div created above a class name
+    taskInfoE1.className = "task-info";
+    
+    //add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>"
+
+    listItemE1.appendChild(taskInfoE1);
+
+    //add entire list item to list
+    tasksToDoE1.appendChild(listItemE1);
+
+};
+
+formE1.addEventListener("submit", taskFormHandler);
